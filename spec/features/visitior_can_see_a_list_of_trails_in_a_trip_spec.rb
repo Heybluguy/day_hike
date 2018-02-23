9a18ba318245539eba2151bbf4e00bd15e467964
heybluguy <heybluguy@yahoo.com>
2018-02-23 09:20:14 -0700

    Initial commit.

---
+++ spec/features/visitior_can_see_a_list_of_trails_in_a_trip_spec.rb
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+
+
+# As a visitor,
+  # when I visit the trips index,
+  # and I click a link to a trip show,
+  # I see a list of trails included in the trip,
+  # I see the name, address, and length for each trail.
