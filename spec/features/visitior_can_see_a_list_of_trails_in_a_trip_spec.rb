c121a035d76c53783b2460e25af56d67c7a60a8b
heybluguy <heybluguy@yahoo.com>
2018-02-23 10:02:26 -0700

    last commit

--- spec/features/visitior_can_see_a_list_of_trails_in_a_trip_spec.rb
+++ spec/features/visitior_can_see_a_list_of_trails_in_a_trip_spec.rb
@@ -1,12 +1,20 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  describe 'when I am on trips index and click on a link to trip show' do
+    scenario 'i see a list of trails included in the trip including name, address and length' do
+      trail = Trail.create(name: "Happy trail", address: "memory lane", length: 1)
+      visit trips_path
+save_and_open_page
+
+      expect(page).to have_content("Happy trail")
 
 
+    end
+  end
+end
 
-
-
-
-
-
-# As a visitor,
+  # As a visitor,
   # when I visit the trips index,
   # and I click a link to a trip show,
   # I see a list of trails included in the trip,
