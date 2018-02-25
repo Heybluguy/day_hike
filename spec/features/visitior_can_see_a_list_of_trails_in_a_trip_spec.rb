7651347844bf805b4f2ce8a09d822d469ed39e4a
heybluguy <heybluguy@yahoo.com>
2018-02-25 13:33:03 -0700

    trip's trials index.

--- spec/features/visitior_can_see_a_list_of_trails_in_a_trip_spec.rb
+++ spec/features/visitior_can_see_a_list_of_trails_in_a_trip_spec.rb
@@ -2,14 +2,20 @@ require 'rails_helper'
 
 describe 'As a visitor' do
   describe 'when I am on trips index and click on a link to trip show' do
-    scenario 'i see a list of trails included in the trip including name, address and length' do
-      trail = Trail.create(name: "Happy trail", address: "memory lane", length: 1)
+    it 'I see a list of trails included in the trip including name, address and length' do
+      winter = Trip.create!(name: "Winter", start_date: "01/02/1985", end_date: "02/05/1985")
+      summer = Trip.create!(name: "Summer", start_date: "07/02/1987", end_date: "08/04/1987")
+      slippery_trail = Trail.create!(length: 5, name: "slippery trail", address: "memory lane")
+      summer.trails << slippery_trail
       visit trips_path
-save_and_open_page
 
-      expect(page).to have_content("Happy trail")
+      expect(page).to have_content("Summer")
+      click_on "Summer"
 
-
+      expect(page).to have_content("Start date: 07/02/1987")
+      expect(page).to have_content("slippery trail")
+      expect(page).to have_content(5)
+      expect(page).to have_content("memory lane")
     end
   end
 end
